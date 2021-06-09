00a3307c01bdf8cd6e6ee2918a43e172edf434c2
Nikolai <nlv.nki@gmail.com>
2021-06-09 17:42:57 +0300

    radded ex. 4

--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,5 +1,15 @@
 'use strict';
 
+const arr = [true, 'hello', 5, 12, -200, false, false, 'word'];
+const hash = { number: 0, string: 0, boolean: 0 };
 
-
+const countTypesInArray = (arr) => {
+  for (const elem of arr) {
+    console.log(typeof elem);
+    let prop = typeof elem;
+    if (hash.hasOwnProperty(prop)) {
+      hash[prop]++;
+    }
+  }
+}
 module.exports = { countTypesInArray };
